8a2ede46bae8de66277b26644bf354a67015836c
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2012-11-13 06:06:16 +0000

    BUGFIX
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/payment_buckaroo/trunk@3615 950b781c-c544-efab-2f99-90083ee69683

--- code/BuckarooPaymentOrderFormExtension.php
+++ code/BuckarooPaymentOrderFormExtension.php
@@ -7,17 +7,29 @@
  */
 class BuckarooPaymentOrderFormExtension extends Extension {
 
-
+	/**
+	 *
+	 * extension check
+	 * return NULL if there is no problem and return true if there is a problem.
+	 */
 	function OrderFormBeforeFinalCalculation($data, $form, $request) {
+		if($data["PaymentMethod"] != "BuckarooPayment") {
+			return null;
+		}
 		$order = ShoppingCart::current_order();
 		//this is the standard way for getting a specific Order Modifier, allowing
 		//you to work on multiple modifiers of the same kind .
-		$modifiers = $order->Modifiers('BuckarooPaymentModifier');
 		if(!isset($data['BuckarooMethod']) || !$data["BuckarooMethod"]) {
 			return "ERROR";
 		}
-		foreach($modifiers as $modifier) {
-			$modifier->updateName(Convert::raw2sql($data['BuckarooMethod'], $write = true));
+		$modifiers = $order->Modifiers('BuckarooPaymentModifier');
+		if($modifiers) {
+			foreach($modifiers as $modifier) {
+				$modifier->updateName(Convert::raw2sql($data['BuckarooMethod'], $write = true));
+			}
+		}
+		else {
+			return "ERROR";
 		}
 		return null;
 	}
